db383b6e155250b45aeb09bb30be8ae5ab0d9245
msidiq2235 <muhsidiq2005@gmail.com>
2024-04-23 17:44:56 +0700

    3

--- PemrogramanDasar2304/PemrogramanDasar2304.cpp
+++ PemrogramanDasar2304/PemrogramanDasar2304.cpp
@@ -6,4 +6,10 @@ class Mahasiswa {
 		string nama;
 		int umur;
 		string jurusan;
+
+		void output() {
+			cout << "Nama: " << nama << endl;
+			cout << "Umur: " << umur << endl;
+			cout << "Jurusan: " << jurusan << endl;
+		}
 };
